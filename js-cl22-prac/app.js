671d33f695ac3ae0b0449e7f0dc9ce1477ec9e9a
Anas Irfan123 <ai8619233@gmail.com>
2023-09-07 15:14:52 +0500

    update code

--- js-cl22-prac/app.js
+++ js-cl22-prac/app.js
@@ -1,1 +1,7 @@
 alert('class 22 learning github (readme Etc)');
+
+console.log('kar li practice github cli and READme file Etc..');
+
+if (true) {
+    alert('yes')
+}
---
+++ js-cl22-pract-folder2/app.js
@@ -0,0 +1,2 @@
+//add next file js practice folder 22 class 
+// and push
